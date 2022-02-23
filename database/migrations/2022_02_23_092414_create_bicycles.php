6ce6852b74e8b20905504e1aa90cb2150f45a9e8
chrs-m <mister.ch@gmail.com>
2022-02-23 10:33:24 +0100

    Add DB's

--- database/migrations/2022_02_23_092414_create_bicycles.php
+++ database/migrations/2022_02_23_092414_create_bicycles.php
@@ -13,10 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('bicycles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('image');
+            $table->integer('quantity');
+            $table->integer('price');
         });
     }
 
@@ -27,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('bicycles');
     }
 };
