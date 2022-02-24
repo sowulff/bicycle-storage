85ddc19d1a78d83b68b011246ac80944ff4e1ce3
chrs-m <mister.ch@gmail.com>
2022-02-24 13:05:07 +0100

    layout + gitignore

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,20 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <title>To Do App</title>
-</head>
+@extends('layouts.basic')
+@section('content')
 
 <section class="">
 <div class="flex flex-col items-center bg-gray-300">
     <p class="text-orange-500">Hello, {{ $user->name }}!</p>
-        <p class="">Do you want to <a href="logout" class="underline text-rose-600">logout</a>?</p>
+    <p class="">Do you want to <a href="logout" class="underline text-rose-600">logout</a>?</p>
 </div>
 
 {{-- @include('errors') --}}
 
 <h1>Hello, {{ $user->name }}!</h1>
-<a href="admin">Add new bike here!</a>
+<a class="underline text-sky-300" href="admin">Add new bike here!</a>
+
+@endsection
