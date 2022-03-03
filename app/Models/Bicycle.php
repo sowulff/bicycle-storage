18ec44b649296b41e6572113f6066d13fdb25936
chrs-m <mister.ch@gmail.com>
2022-03-03 15:11:41 +0100

    update price function/model

--- app/Models/Bicycle.php
+++ app/Models/Bicycle.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,4 +16,12 @@ class Bicycle extends Model
         'price',
         'quantity',
     ];
+
+    protected function price(): Attribute
+    {
+        return new Attribute(
+            get: fn ($value) => number_format($value, 0, ".", "."),
+            set: fn ($value) => $value,
+        );
+    }
 }
