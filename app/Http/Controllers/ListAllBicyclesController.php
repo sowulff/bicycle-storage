c8e8239c9e03577a1e7c9c4e60dd565844fd77d8
chrs-m <mister.ch@gmail.com>
2022-03-03 15:00:21 +0100

    View all bike page + overall design and fixes

--- app/Http/Controllers/ListAllBicyclesController.php
+++ app/Http/Controllers/ListAllBicyclesController.php
@@ -15,7 +15,8 @@ class ListAllBicyclesController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $bicycles = Bicycle::select('name', 'price')->get();
+        // $bicycles = Bicycle::select('name', 'price', 'image')->get();
+        $bicycles = Bicycle::all();
 
         return view('bicycles', ['bicycles' => $bicycles]);
     }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,18 @@
 @extends('layouts.basic')
 @section('content')
 
-<section class="">
-<div class="flex flex-col items-center bg-gray-300">
-    <p class="text-orange-500">Hello, {{ $user->name }}!</p>
-    <p class="">Do you want to <a href="logout" class="underline text-rose-600">logout</a>?</p>
-</div>
+<div class="shadow-2xl p-12 mt-32 max-w-sm mx-auto rounded">
+    <h1 class="mb-2 text-3xl text-center">
+      BicycleStorage 2.0
+    </h1>
+    <h3 class="text-xl mb-4 text-center">Hello, {{ $user->name }}!</h3>
 
-{{-- @include('errors') --}}
+    <div class="flex flex-col">
+        <a class="underline text-sky-300 hover:text-sky-500" href="admin">Add new bike here!</a>
+        <a class="underline text-sky-300 hover:text-sky-500" href="bicycles/all">See all our bikes here!</a>
 
-<h1>Hello, {{ $user->name }}!</h1>
-<a class="underline text-sky-300" href="admin">Add new bike here!</a>
-<a class="underline text-sky-300" href="bicycles/all">See all our bikes here!</a>
-
-
+        <p class="">Do you want to <a href="logout" class="underline text-rose-400 hover:text-rose-600">logout</a>?</p>
+    </div>
+  </div>
+@include('errors')
 @endsection
