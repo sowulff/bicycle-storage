2d6d320665f48eb9e144e488ea8076b63ea06f91
DavisDavisDavis <olizoooom@gmail.com>
2022-03-13 21:37:22 +0100

    Fixed bicycle view to match favorite with the correct id 🎯🆔
    Both id of the bicycle and user id

--- resources/views/bicycles.blade.php
+++ resources/views/bicycles.blade.php
@@ -3,20 +3,24 @@
 
 <div class="shadow-2xl p-12 mt-32 max-w-sm mx-auto rounded flex flex-col">
     <h2 class="text-3xl mx-auto">Here are all our bikes!</h2>
-    @for ($i = 0; $i < Count($bicycles); $i++)
+    @foreach ($bicycles as $bicycle)
+        <?php $display_favorite = true; ?>
     <div class="shadow-sm p-12 min-w-[300px] max-w-sm mx-auto rounded flex flex-col mt-4">
-        <p class="text-xl pb-2">{{$bicycles[$i]->name . " " . '(' . $bicycles[$i]->price . 'kr)'}}</p>
-        <img src="{{ $bicycles[$i]->image }}" alt="">
-        <a href="buy/{{$bicycles[$i]->id}}">Select bike</a>
-        @if (isset($wishlist[$i]->favorite) && $wishlist[$i]->favorite)
-            <p>{{$wishlist[$i]->favorite}}</p>
-            <a href="removeFavorite/{{$wishlist[$i]->id}}">Remove favorite</a>
-        @else
-            <a href="favorite/{{$bicycles[$i]->id}}">Add to favourite</a>
+        <p class="text-xl pb-2">{{$bicycle->name . " " . '(' . $bicycle->price . 'kr)'}}</p>
+        <img src="{{ $bicycle->image }}" alt="">
+        <a href="buy/{{$bicycle->id}}">Select bike</a>
+        @foreach ($wishlist as $wishitem)
+            @if ($wishitem->favorite && $wishitem->bicycle_id == $bicycle->id && $wishitem->user_id == Auth::id())
+                <a href="removeFavorite/{{$wishitem->id}}">Remove from favorites ❌</a>
+                <?php $display_favorite = false; ?>
+                @break
+            @endif
+        @endforeach
+        @if ($display_favorite)
+            <a href="favorite/{{$bicycle->id}}">Add to favorites ✅</a>
         @endif
     </div>
-    @endfor
-    <a href="cartView">Go to cart</a>
+    @endforeach
     <a class="hover:underline hover:text-gray-500 mt-8" href="/dashboard">&larr; Back</a>
 </div>
 @include('errors')
