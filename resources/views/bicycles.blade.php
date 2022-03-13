c01244460c1616dd36bbc314cbc156a14acd3b52
DavisDavisDavis <olizoooom@gmail.com>
2022-03-13 12:19:01 +0100

    Updated bicycles view to display favorite bikes 💖
    Changed from an foreach loop to a for loop.

--- resources/views/bicycles.blade.php
+++ resources/views/bicycles.blade.php
@@ -3,14 +3,17 @@
 
 <div class="shadow-2xl p-12 mt-32 max-w-sm mx-auto rounded flex flex-col">
     <h2 class="text-3xl mx-auto">Here are all our bikes!</h2>
-    @foreach ($bicycles as $bicycle)
+    @for ($i = 0; $i < Count($bicycles); $i++)
     <div class="shadow-sm p-12 min-w-[300px] max-w-sm mx-auto rounded flex flex-col mt-4">
-        <p class="text-xl pb-2">{{$bicycle->name . " " . '(' . $bicycle->price . 'kr)'}}</p>
-        <img src="{{ $bicycle->image }}" alt="">
-        <a href="buy/{{$bicycle->id}}">Select bike</a>
-        <a href="favorite/{{$bicycle->id}}">Add to favourite</a>
+        <p class="text-xl pb-2">{{$bicycles[$i]->name . " " . '(' . $bicycles[$i]->price . 'kr)'}}</p>
+        <img src="{{ $bicycles[$i]->image }}" alt="">
+        <a href="buy/{{$bicycles[$i]->id}}">Select bike</a>
+        <a href="favorite/{{$bicycles[$i]->id}}">Add to favourite</a>
+        @if (isset($wishlist[$i]->favorite))
+            <p>{{$wishlist[$i]->favorite}}</p>
+        @endif
     </div>
-    @endforeach
+    @endfor
     <a href="cartView">Go to cart</a>
     <a class="hover:underline hover:text-gray-500 mt-8" href="/dashboard">&larr; Back</a>
 </div>
