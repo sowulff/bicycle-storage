214f28a477533d76c61532d9cdb8280d4e359ad8
DavisDavisDavis <olizoooom@gmail.com>
2022-03-15 21:38:20 +0100

    Require on OrderBicycles to not validate negative numbers ❄️❌

--- app/Http/Controllers/OrderBicycleController.php
+++ app/Http/Controllers/OrderBicycleController.php
@@ -11,6 +11,10 @@ class OrderBicycleController extends Controller
 {
     public function __invoke(Request $request, $id)
     {
+        $this->validate($request, [
+            'quantity' => 'required|int|min:0',
+        ]);
+
         $bicycle = Bicycle::find($id);
         if ($request->input('quantity') > $bicycle->quantity) {
             return back()->with('fail', 'Opps you are ordering too many bicycles!');
