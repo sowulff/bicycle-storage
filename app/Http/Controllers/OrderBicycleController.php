1a3896e4fffe072c906b85ce33442304311b5f5d
DavisDavisDavis <olizoooom@gmail.com>
2022-03-15 21:28:29 +0100

    Fixed negative numbers in Cart 🥶🛒

--- app/Http/Controllers/OrderBicycleController.php
+++ app/Http/Controllers/OrderBicycleController.php
@@ -5,11 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\Bicycle;
 use Illuminate\Http\Request;
 
+use function PHPUnit\Framework\returnSelf;
+
 class OrderBicycleController extends Controller
 {
     public function __invoke(Request $request, $id)
     {
         $bicycle = Bicycle::find($id);
+        if ($request->input('quantity') > $bicycle->quantity) {
+            return back()->withErrors(['invalidQuantity'=>'Opps you are ordering too many bicycls!']);
+        }
         $bicycle->quantity -= $request->input('quantity');
         $bicycle->update();
 
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -5,13 +5,20 @@
 <div class="shadow-xl p-12 min-w-[300px] max-w-sm mx-auto rounded flex flex-col mt-4">
     <a class="hover:underline hover:text-gray-500 mb-4" href="{{ route('bicycleAll') }}">&larr; Back</a>
 
+    @if (\Session::has('invalidQuantity'))
+    <div class="bg-green-400 py-2 mt-2 rounded-md">
+        <p>{{session('errors')->first('invalidQuantity');}}</p>
+    </div>
+    @endif
+
+
     <p class="text-xl pb-2 mt-14">{{$bicycle->name . " " . '(' . $bicycle->price . 'kr)'}}</p>
     <p>{{$bicycle->quantity . ' st'}}</p>
     <img src="{{ $bicycle->image }}" alt="">
     <form action="{{ route('orderBike', $bicycle)}}" method="POST">
         @csrf
         <label for="quantity" class="block mt-2 w-56 text-slate-700">Quantity:</label>
-        <input type="number" id="quantity" name="quantity" class="rounded-md mb-2 border-2 pl-1 text-sm w-56" value="1">
+        <input type="number" id="quantity" name="quantity" class="rounded-md mb-2 border-2 pl-1 text-sm w-56" value="1" min="0">
 
         <button>Make order</button>
     </form>
