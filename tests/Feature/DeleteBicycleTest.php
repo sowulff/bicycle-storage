fe4f668babdecef5f938b4728c466951db8afff5
sowulff <sowu99@gmail.com>
2022-03-15 09:02:31 +0100

    added test

---
+++ tests/Feature/DeleteBicycleTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Bicycle;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class DeleteBicycleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_delete_bicycle()
+    {
+        // Create admin
+        $user = new User();
+        $user->name = 'admin';
+        $user->email = 'admin@admin.com';
+        $user->password = Hash::make('123');
+        $user->is_admin = true;
+        $user->save();
+
+        $bicycle = Bicycle::factory()->create();
+
+        $request = $this
+            ->actingAs($user)
+            ->followingRedirects()
+            ->delete("deleteBicycle/{bicycle->id}");
+
+        $this->assertDatabaseHas('bicycles', $bicycle->toArray());
+        $request->assertSeeText("Bicycle has been deleted!");
+        $request->assertStatus(200);
+    }
+}
