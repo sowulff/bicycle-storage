c3d780369dbf03652fbaa79cebd1af247719d26c
chrs-m <mister.ch@gmail.com>
2022-03-15 14:15:15 +0100

    validate new details for users

--- app/Http/Controllers/EditUserController.php
+++ app/Http/Controllers/EditUserController.php
@@ -10,10 +10,19 @@ class EditUserController extends Controller
 {
     public function __invoke(Request $request, $id)
     {
+        $data = request()->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['nullable', 'string', 'min:8']
+        ]);
+
+
         $user = User::find($id);
-        $user->name = $request->input('name');
-        $user->email = $request->input('email');
-        $user->password = bcrypt($request->input('password'));
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        if ($data['password'] != null) {
+            $user->password = bcrypt($request->input('password'));
+        }
         $user->update();
         return redirect()->back()->with('status', 'User updated successfully!');
     }
